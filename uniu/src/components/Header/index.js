6dfd4f02184b722087ba7c7d21b808c755474686
sfal <s.falchieri@gmail.com>
2020-10-10 20:13:05 +0200

    Add fake header

--- uniu/src/components/Header/index.js
+++ uniu/src/components/Header/index.js
@@ -6,27 +6,30 @@ import Toggle from "@components/Toggle"
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <a className="logo" href="/">
-          <Logo />
-        </a>
-        <ul>
-          <li>
-            <a href="/">Discover</a>
-          </li>
-          <li>
-            <a href="/">Features</a>
-          </li>
-          <li>
-            <Toggle />
-          </li>
-          <li>
-            <a href="/">Download</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header>
+        <nav>
+          <a className="logo" href="/">
+            <Logo />
+          </a>
+          <ul>
+            <li>
+              <a href="/">Discover</a>
+            </li>
+            <li>
+              <a href="/">Features</a>
+            </li>
+            <li>
+              <Toggle />
+            </li>
+            <li>
+              <a href="/">Download</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="fake-header"></div>
+    </>
   )
 }
 
