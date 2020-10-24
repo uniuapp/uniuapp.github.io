c39a4fb040e902aefb5010e94bc6008d84689419
sfal <s.falchieri@gmail.com>
2020-10-24 13:21:34 +0200

    Add custom carousel

--- uniu/src/components/Features/index.js
+++ uniu/src/components/Features/index.js
@@ -1,21 +1,22 @@
 import "./style.css"
-import Slider from "react-slick"
 
-import React from "react"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import React, { useEffect, useRef, useState } from "react"
+
 import ScreenOrari from "@assets/screen-orari.png"
 import ScreenEsami from "@assets/screen-esami.png"
 import ScreenPrevisioni from "@assets/screen-previsioni.png"
 
 const Features = () => {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  }
+  const itemRef = useRef()
+  const carouselRef = useRef()
+  const containerRef = useRef()
+
+  const [index, setIndex] = useState(0)
+
+  const [nextDisabled, setNextDisabled] = useState(false)
+  const [prevDisabled, setPrevDisabled] = useState(true)
+
+  const [carouselWidth, setCarouselWidth] = useState()
 
   const slides = [
     { title: "slide 1", paragraph: "this is a paragraph", screen: ScreenOrari },
@@ -27,19 +28,68 @@ const Features = () => {
     },
   ]
 
-  return (
-    <Slider {...settings}>
-      {slides.map((item, index) => (
-        <div key={index} className="slides" style={{ display: "flex" }}>
-          <div className="slides__screen-wrapper">
-            <img src={item.screen} />
-          </div>
+  const container = document.querySelector(".carousel__inner-container")
 
-          <h1>{item.title}</h1>
-          <p>{item.paragraph}</p>
+  useEffect(() => {
+    setCarouselWidth(carouselRef.current.offsetWidth)
+  })
+
+  const nextSlide = () => {
+    const translate = carouselWidth * (index + 1)
+    container.style.transform = `translateX(${-translate}px)`
+    const nextIndex = index + 1
+    setIndex(nextIndex)
+
+    if (nextIndex == slides.length - 1) {
+      setNextDisabled(true)
+    }
+    setPrevDisabled(false)
+  }
+  const prevSlide = () => {
+    const translate = carouselWidth * (index - 1)
+    container.style.transform = `translateX(${-translate}px)`
+
+    const nextIndex = index - 1
+    setIndex(nextIndex)
+
+    if (nextIndex == 0) {
+      setPrevDisabled(true)
+    }
+    setNextDisabled(false)
+  }
+
+  return (
+    <>
+      <div className="carousel" ref={carouselRef}>
+        <div
+          className="carousel__inner-container"
+          ref={containerRef}
+          style={{ width: carouselWidth && carouselWidth * slides.length }}
+        >
+          {slides.map((item, i) => (
+            <div
+              className="carousel__item"
+              key={i}
+              ref={itemRef}
+              style={{
+                width: carouselWidth,
+                backgroundColor: i === 0 ? "blue" : i === 1 ? "red" : "yellow",
+              }}
+            >
+              <p>ciao</p>
+            </div>
+          ))}
         </div>
-      ))}
-    </Slider>
+      </div>
+      <div className="carousel__controls">
+        <button disabled={prevDisabled} onClick={prevSlide}>
+          prev
+        </button>
+        <button disabled={nextDisabled} onClick={nextSlide}>
+          next
+        </button>
+      </div>
+    </>
   )
 }
 
