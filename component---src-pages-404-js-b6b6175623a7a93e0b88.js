79893f8f90af3efac2c1bdc901abc2cea351669a
sfal <s.falchieri@gmail.com>
2021-10-03 20:17:39 +0200

    Updates

---
+++ component---src-pages-404-js-b6b6175623a7a93e0b88.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("Zttt"),u=n("q1tI"),l=n.n(u),o=n("vrFN");e.default=function(){return l.a.createElement(a.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"404: Not Found"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b6b6175623a7a93e0b88.js.map
